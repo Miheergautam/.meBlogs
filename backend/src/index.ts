71231f2f0d015774c62fdf5706a123251baadcc7
Miheer Gautam <miheergautam04@gmail.com>
2025-01-25 16:17:34 +0530

    fix credientials

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,5 +6,3 @@ const app = new Hono();
 app.route("/api/v1", mainRoute);
 
 export default app;
-
-//postgresql://neondb_owner:npg_Bi2QOFnMZw6W@ep-noisy-rice-a4e2jjm1-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require
