a034eae6cd2663337985cc2c4999bf36df75045c
Miheer Gautam <miheergautam04@gmail.com>
2025-03-19 10:02:20 +0530

    fix: enabled cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import mainRoute from "./routes/mainRoute";
 
 const app = new Hono();
 
+app.use(cors());
 app.route("/api/v1", mainRoute);
 
 export default app;
